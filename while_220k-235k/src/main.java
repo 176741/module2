068739783632f415a6dfbc8e3449b0adc9b03a74
176741 <60668637+176741@users.noreply.github.com>
2020-02-04 23:15:28 +0300

    Update main.java

--- while_220k-235k/src/main.java
+++ while_220k-235k/src/main.java
@@ -2,10 +2,15 @@ public class main
 {
     public static void main(String[] args)
     {
-        int i = 220000;
-        while (i >= 220000 && i <= 235000) {
+        int i = 200000;
+        while (i >= 200000 && i <= 210000) {
             System.out.println("номер билета " + i);
             i++;
+        }
+        int g = 220000;
+        while (g >= 220000 && g <= 235000) {
+            System.out.println("номер билета " + g);
+            g++;
         }
     }
 }
