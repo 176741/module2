84025537bbbdb868b36b59c6726d551b0f18c2ce
176741 <60668637+176741@users.noreply.github.com>
2020-02-04 21:53:20 +0300

    Add files via upload

---
+++ while_220k-235k/src/main.java
@@ -0,0 +1,11 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        int i = 220000;
+        while (i >= 220000 && i <= 235000) {
+            System.out.println("номер билета " + i);
+            i++;
+        }
+    }
+}
