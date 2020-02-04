14fae39798e04d5a5b507e8507d88eece83f2eaa
176741 <60668637+176741@users.noreply.github.com>
2020-02-04 21:52:42 +0300

    Add files via upload

---
+++ for_200k-210k/src/main.java
@@ -0,0 +1,10 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        for(int i = 200000; i <= 210000; ++i)
+        {
+            System.out.println("номер билета " + i);
+        }
+    }
+}
